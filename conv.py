e9e9c7bc5e20045d60b430917c9a13f7cd2fd5fb
Chris Koss <chris@mckoss.com>
2014-02-05 16:03:13 -0500

    changing lesson structore to accommodate new lesson"

--- conv.py
+++ conv.py
@@ -3,43 +3,119 @@ from pprint import pprint
 json_data = open('lesson.json')
 data = json.load(json_data)
 json_data.close()
-#pprint(data['msgChunks'][0])
-#x = raw_input()
-#print x
 
-print "Welcome to " + data['course']
+SLEEPTIME = 0 #how long to wait between waitMsg requests in secs
 
-print "Text L for a lesson or S for settings"
-x = raw_input()
-while(x.lower() != "l"):
-    print "Settings have not been implemented, just text L to continue"
-    x = raw_input()
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import TwilioRestClient
+import time
 
-for msgChunk in data['msgChunks']:
-    for prompt in msgChunk['prompts']:
-        print(prompt)
-    if msgChunk['type'] == 'info':
-        x = ""
-        while(x.lower() != "c"):
-            print("Text C to continue.")
-            x = raw_input()
-    elif msgChunk['type'] == 'multi':
-        x = ""
-        correct = ""
-        for response in msgChunk['responses']:            
-            if msgChunk['responses'][response]['correct']:
-                correct = response
-        x = raw_input().lower()
-        while(x != correct):
-            if(x == "a" or x == "b" or x == "c" or x == "d"):
-                print(msgChunk['responses'][x]['prompts'][0])
-            else:
-                print "Try again, choosing from A, B, C, or D."
-            x = raw_input().lower()
-        if x == correct:
-            print(msgChunk['responses'][x]['prompts'][0])
-        
-        
+# Your Account Sid and Auth Token from twilio.com/user/account
+ACCOUNT_SID = "AC94743d49300d49a696cf09bfef229c82"
+AUTH_TOKEN  = "459bb4d749bdff002e45d44d33a4ad7f"
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
 
 
-print "Done!"
+# chris - "4252467703"
+# james - "2068490631"
+PHONENUM = "4252467703"
+msgCount = 0
+
+def waitForNewMessage():
+    print "Waiting for new message"
+    messages = getMessages()
+    msgCount = messages.count()
+    while True:
+        messages = getMessages()
+        if isNewMsg(msgCount, messages):
+            print "Message found",
+            print messages.list()[0].body
+            return messages.list()[0].body
+        time.sleep(SLEEPTIME)
+
+def confirmMessageSent(body):
+    messages = getMessages()
+    while messages.list()[0].body != body:
+        print "last message not new: " + messages.list()[0].body + "| should be " + body
+        #time.sleep(1);
+        messages = getMessages()
+    print "Confirmed body: " + messages.list()[0].body
+    return True
+
+
+
+def getMessages():
+   
+    # A list of message objects with the properties described above
+    #try:
+    print "Requesting messages"
+    client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+    request = client.messages
+    #except:
+     #   print "Unexpected error", sys.exc_info()[0]
+    #result = cl
+    return request
+
+
+def isNewMsg(msgCount, messages):
+    if messages.count() > msgCount:
+        #print "msgCount ", 
+        #print msgCount
+        #print "messages.count() ",
+        #print messages.count()
+        #print "messages.list()[1].body ",
+        #print messages.list()[1].body
+        return True
+    else:
+        #print "No new messages"
+        return False
+
+def sendMessage(body):
+    print "sending message: ",
+    print body
+    time.sleep(1)    
+    client.messages.create(
+	    to=PHONENUM, 
+	    from_="+12067454564",
+        body=body   
+    )
+    confirmMessageSent(body)
+
+def runLesson():
+    #sendMessage("Welcome to " + data['course'])
+#
+ #   sendMessage("Text L for a lesson or S for settings")
+  #  x = waitForNewMessage()
+   # while(x.lower() != "l"):
+    #    sendMessage("Settings have not been implemented, just text L to continue")
+     #   x = waitForNewMessage()
+
+    for msgChunk in data['msgChunks']:
+        for prompt in msgChunk['prompts']:
+            sendMessage(prompt)
+        if msgChunk['type'] == 'info':
+            x = ""
+            while(x.lower() != "c"):
+                sendMessage("Text C to continue.")
+                x = waitForNewMessage()
+        elif msgChunk['type'] == 'multi':
+            x = ""
+            correct = ""
+            for response in msgChunk['responses']:            
+                if msgChunk['responses'][response]['correct']:
+                    correct = response
+            x = waitForNewMessage().lower()
+            while(x != correct):
+                if(x == "a" or x == "b" or x == "c" or x == "d" or x == "e" or x == "h"):
+                    sendMessage(msgChunk['responses'][x]['prompts'][0])
+                else:
+                    sendMessage("Try again, choosing from A, B, C, or D.")
+                x = waitForNewMessage().lower()
+            if x == correct:
+                sendMessage(msgChunk['responses'][x]['prompts'][0])
+           
+    sendMessage("You have completed Lesson " + str(data['lessonnum']) + ": " + data['name'] + ". Text one of the following options.")
+    sendMessage("L - Lesson\nQ - Quiz\nG - Grades\nP - Points\nS - Settings")
+    print "Done!"
+
+runLesson()
