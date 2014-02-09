ca805b88d085b497cc8bdf84b13dc35fe1d25b5e
Chris Koss <chris@mckoss.com>
2014-02-09 12:13:40 -0800

    passing third test

--- engine.py
+++ engine.py
@@ -10,6 +10,13 @@ class Engine:
     def process_message(self, state=0, message=""):
         logging.info("Called with state=%s and message='%s'", str(state), message)
         messages = []
+        
+        if self.lesson['msgChunks'][state]['type'] == "multi":
+           if self.lesson['msgChunks'][state]['responses'][message]:
+                messages += self.lesson['msgChunks'][state]['responses'][message]['prompts']
+                if message not in self.lesson['msgChunks'][state]["correctResponses"]:
+                    return state, messages
+
         while True:
             messages += self.lesson['msgChunks'][state]['prompts']
             if self.lesson['msgChunks'][state]['type'] == "multi":
