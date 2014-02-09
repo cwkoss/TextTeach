3a19155596aa95fa41d3018dc3b1e9c81e96d7ac
Mike Koss <mckoss@startpad.org>
2014-02-08 18:30:10 -0800

    fix indentation and run pep8 tests

--- engine.py
+++ engine.py
@@ -2,19 +2,18 @@ import logging
 
 logging.getLogger().setLevel(logging.INFO)
 
+
 class Engine:
-	def __init__(self, lesson):
-		self.lesson = lesson
+    def __init__(self, lesson):
+        self.lesson = lesson
 
-	def process_message(self, state=0, message=""):
-		logging.info("Called with state=%s and message='%s'", str(state), message)
-		#messages = 
+    def process_message(self, state=0, message=""):
+        logging.info("Called with state=%s and message='%s'", str(state), message)
+        messages = []
+        while True:
+            messages += self.lesson['msgChunks'][state]['prompts']
+            if self.lesson['msgChunks'][state]['type'] == "multi":
+                break
+            state += 1
 
-		messages = []
-		while True:
-			messages += self.lesson['msgChunks'][state]['prompts']
-			if self.lesson['msgChunks'][state]['type'] == "multi":
-				break
-			state += 1
-
-		return state, messages
+        return state, messages
