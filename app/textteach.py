6bb2e449d3a202acdecde4eb25b82b80534743e4
Mike Koss <mckoss@startpad.org>
2014-02-09 14:37:34 -0800

    stub appengine with twillio

--- app/textteach.py
+++ app/textteach.py
@@ -1,12 +1,24 @@
 import webapp2
+from google.appengine.api import memcache
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write("Hello, student.")
+        self.response.write(memcache.get(key='body'))
+
+
+class ReceiveSMS(webapp2.RequestHandler):
+    def get(self):
+        self.post()
+
+    def post(self):
+        message = self.request.get('Body')
+        memcache.set(key="body", value=message)
 
 
 application =webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/sms', ReceiveSMS)
 ], debug=True)
